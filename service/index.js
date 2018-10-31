38253aaaceed0158d37c9da3f121186451134c5a
zhudong <503749378@qq.com>
2018-10-31 11:48:07 +0800

    加入koa-logger

--- service/index.js
+++ service/index.js
@@ -5,9 +5,11 @@ const mongoose = require('mongoose')
 const bodyParser = require('koa-bodyparser')
 const cors = require('koa2-cors')
 const Router = require('koa-router')
+const logger = require('koa-logger');
 
 app.use(bodyParser())
 app.use(cors())
+app.use(logger());
 
 let user = require('./appApi/user.js')
 let home = require('./appApi/home.js')
